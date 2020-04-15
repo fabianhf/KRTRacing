2b10b50c45d7d45d549d8a155ed15ec66815bb87
fabianhf <fabian.haas-fickinger@rwth-aachen.de>
2020-04-15 11:51:48 +0200

    Added ICLOCS, Adigator and Init.m for Controller

--- matfiles/Controller/extractCenterline.m
+++ matfiles/Controller/extractCenterline.m
@@ -1,14 +1,8 @@
-function [centerLine,n] = extractCenterline(pathToM)
+function [centerLine,n] = extractCenterline(t_r,t_l)
 %EXTRACTCENTERLINE Summary of this function goes here
 
-try
-    track = load(pathToM);
-catch
-    error("Das ist Mist");
-end
-
-pointDifference = track.t_r-track.t_l;
-centerLine = track.t_l+pointDifference*0.5;
+pointDifference = t_r-t_l;
+centerLine = t_l+pointDifference*0.5;
 n = norm(pointDifference(1,:)*0.5);
 
 end
