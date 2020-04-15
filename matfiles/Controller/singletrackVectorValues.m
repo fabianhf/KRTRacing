fc98287c259b0f8f7242dca2c42f5c0f11353181
Steffen Zeile <steffen@zeilehome.de>
2020-04-15 14:33:33 +0200

    Code cosmetics

--- matfiles/Controller/singletrackVectorValues.m
+++ matfiles/Controller/singletrackVectorValues.m
@@ -33,27 +33,27 @@ function [X_dot] = singletrackVectorValues(t, X, U)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% vehicle parameters
-m=1239; % vehicle mass
-g=9.81; % gravitation
-l_f=1.19016; % distance of the front wheel to the center of mass 
-l_r=1.37484; % distance of the rear wheel to the center of mass
-%l=l_f+l_r; % vehicle length (obsolete)
-R=0.302; % wheel radius
-I_z=1752; % vehicle moment of inertia (yaw axis)
-I_R=1.5; % wheel moment of inertia
-i_g=[3.91 2.002 1.33 1 0.805]; % transmissions of the 1st ... 5th gear
-i_0=3.91; % motor transmission
-B_f=10.96; % stiffnes factor (Pacejka) (front wheel)
-C_f=1.3; % shape factor (Pacejka) (front wheel)
-D_f=4560.4; % peak value (Pacejka) (front wheel)
-E_f=-0.5; % curvature factor (Pacejka) (front wheel)
-B_r=12.67; %stiffnes factor (Pacejka) (rear wheel)
-C_r=1.3; %shape factor (Pacejka) (rear wheel)
-D_r=3947.81; %peak value (Pacejka) (rear wheel)
-E_r=-0.5; % curvature factor (Pacejka) (rear wheel)
-f_r_0=0.009; % coefficient (friction)
-f_r_1=0.002; % coefficient (friction)
-f_r_4=0.0003; % coefficient (friction)
+m = 1239; % vehicle mass
+g = 9.81; % gravitation
+l_f = 1.19016; % distance of the front wheel to the center of mass 
+l_r = 1.37484; % distance of the rear wheel to the center of mass
+%l = l_f+l_r; % vehicle length (obsolete)
+R = 0.302; % wheel radius
+I_z = 1752; % vehicle moment of inertia (yaw axis)
+I_R = 1.5; % wheel moment of inertia
+i_g = [3.91 2.002 1.33 1 0.805]; % transmissions of the 1st ... 5th gear
+i_0 = 3.91; % motor transmission
+B_f = 10.96; % stiffnes factor (Pacejka) (front wheel)
+C_f = 1.3; % shape factor (Pacejka) (front wheel)
+D_f = 4560.4; % peak value (Pacejka) (front wheel)
+E_f = -0.5; % curvature factor (Pacejka) (front wheel)
+B_r = 12.67; %stiffnes factor (Pacejka) (rear wheel)
+C_r = 1.3; %shape factor (Pacejka) (rear wheel)
+D_r = 3947.81; %peak value (Pacejka) (rear wheel)
+E_r = -0.5; % curvature factor (Pacejka) (rear wheel)
+f_r_0 = 0.009; % coefficient (friction)
+f_r_1 = 0.002; % coefficient (friction)
+f_r_4 = 0.0003; % coefficient (friction)
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% control inputs
@@ -63,30 +63,30 @@ F_b = U(:, 3); %braking force
 zeta = U(:, 4); % braking force distribution
 phi = U(:, 5); % gas pedal position
 % input constraints
-if delta>0.53 % upper bound for steering angle exceeded?
-    delta=0.53; % upper bound for steering angle
-end
-if delta<-0.53 % lower bound for steering angle exceeded?
-    delta=-0.53; % lower bound for steering angle
-end
-if F_b<0 % lower bound for braking force exceeded?
-    F_b=0; % lower bound for braking force
-end
-if F_b>15000 % upper bound for braking force exceeded?
-    F_b=15000; % upper bound for braking force 
-end
-if zeta<0 % lower bound for braking force distribution exceeded?
-    zeta=0; % lower bound for braking force distribution 
-end
-if zeta>1 % upper bound for braking force distribution exceeded?
-    zeta=1; % upper bound for braking force distribution
-end
-if phi<0 % lower bound for gas pedal position exceeded?
-    phi=0; % lower bound for gas pedal position
-end
-if phi>1 % upper bound for gas pedal position exceeded?
-    phi=1; % upper bound for gas pedal position
-end
+% if delta > 0.53 % upper bound for steering angle exceeded?
+%     delta = 0.53; % upper bound for steering angle
+% end
+% if delta < -0.53 % lower bound for steering angle exceeded?
+%     delta = -0.53; % lower bound for steering angle
+% end
+% if F_b < 0 % lower bound for braking force exceeded?
+%     F_b = 0; % lower bound for braking force
+% end
+% if F_b > 15000 % upper bound for braking force exceeded?
+%     F_b = 15000; % upper bound for braking force 
+% end
+% if zeta < 0 % lower bound for braking force distribution exceeded?
+%     zeta = 0; % lower bound for braking force distribution 
+% end
+% if zeta > 1 % upper bound for braking force distribution exceeded?
+%     zeta = 1; % upper bound for braking force distribution
+% end
+% if phi < 0 % lower bound for gas pedal position exceeded?
+%     phi = 0; % lower bound for gas pedal position
+% end
+% if phi > 1 % upper bound for gas pedal position exceeded?
+%     phi = 1; % upper bound for gas pedal position
+% end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% state vector
@@ -107,8 +107,8 @@ varphi_dot = X(:, 10); % wheel rotary frequency
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% slip
 %slip angles and steering
-a_f = delta - atan((l_f * psi_dot - v .* sin(beta))./(v .* cos(beta))); % front slip angle
-a_r = atan((l_r * psi_dot + v .* sin(beta))./(v .* cos(beta))); % rear slip angle
+a_f = delta - atan((l_f * psi_dot - v .* sin(beta)) ./ (v .* cos(beta))); % front slip angle
+a_r = atan((l_r * psi_dot + v .* sin(beta)) ./ (v .* cos(beta))); % rear slip angle
 %if af>ar %understeering?
 %steering='understeering';
 %end
@@ -134,18 +134,18 @@ S = 0; % neglect wheel slip
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% traction, friction, braking
-n = v .* transpose(i_g(G)) * i_0 * (1/(1 - S))/R; % motor rotary frequency
+n = v .* transpose(i_g(G)) * i_0 * (1./(1 - S))/R; % motor rotary frequency
 n(isnan(n)) = 0; % rotary frequency well defined? > recover rotary frequency
 
 n(n > (4800*pi)/30) = (4800*pi)/30; % maximal rotary frequency exceeded? > recover maximal rotary frequency
 
 T_M = 200 * phi .* (15 - 14 * phi) ...
-    - 200 * phi .* (15 - 14 * phi) .* (((n * (30/pi)).^(5 * phi))./(4800.^(5 * phi))); % motor torque
+    - 200 * phi .* (15 - 14 * phi) .* (((n * (30 / pi)).^(5 * phi)) ./ (4800.^(5 * phi))); % motor torque
 M_wheel = transpose(i_g(G)) .* i_0 .* T_M; % wheel torque
-F_w_r = (m * l_f * g)./(l_f + l_r); % weight rear
-F_w_f = (m * l_r * g)./(l_f + l_r); % weight front
-f_r = f_r_0 + f_r_1 * (abs(v) * 3.6)/100 ...
-    + f_r_4 * ((abs(v) * 3.6)/100).^4; % approximate friction
+F_w_r = (m * l_f * g) ./ (l_f + l_r); % weight rear
+F_w_f = (m * l_r * g) ./ (l_f + l_r); % weight front
+f_r = f_r_0 + f_r_1 * (abs(v) * 3.6) / 100 ...
+    + f_r_4 * ((abs(v) * 3.6) / 100).^4; % approximate friction
 F_b_r = zeta .* F_b; % braking force rear
 F_b_f = F_b .* (1 - zeta); % braking force front
 F_f_r = f_r .* F_w_r; % friction rear
@@ -169,17 +169,17 @@ y_dot = v .* sin(psi - beta); % lateral velocity
 v_dot = (F_x_r .* cos(beta) + F_x_f .* cos(delta + beta) - F_y_r .* sin(beta) ...
       - F_y_f .* sin(delta + beta))/m; % acceleration
 beta_dot = omega - (F_x_r .* sin(beta) + F_x_f .* sin(delta + beta) + F_y_r .* cos(beta) ...
-         + F_y_f .* cos(delta + beta)) ./(m * v); % side slip rate
+         + F_y_f .* cos(delta + beta)) ./ (m * v); % side slip rate
 psi_dot = omega; % yaw rate
 omega_dot = (F_y_f .* l_f .* cos(delta) - F_y_r .* l_r ...
           + F_x_f .* l_f .* sin(delta)) / I_z; % yaw angular acceleration
 x_dot_dot = (F_x_r .* cos(psi) + F_x_f .* cos(delta + psi) - F_y_f .* sin(delta + psi) ...
-          - F_y_r .* sin(psi))/m; % longitudinal acceleration
+          - F_y_r .* sin(psi)) / m; % longitudinal acceleration
 y_dot_dot = (F_x_r .* sin(psi) + F_x_f .* sin(delta + psi) + F_y_f .* cos(delta + psi) ...
-          + F_y_r .* cos(psi))/m; % lateral acceleration
+          + F_y_r .* cos(psi)) / m; % lateral acceleration
 psi_dot_dot = (F_y_f .* l_f .* cos(delta) - F_y_r .* l_r ...
-            + F_x_f .* l_f .* sin(delta))/I_z; % yaw angular acceleration
-varphi_dot_dot = (F_x_r * R)/I_R; % wheel rotary acceleration
+            + F_x_f .* l_f .* sin(delta)) / I_z; % yaw angular acceleration
+varphi_dot_dot = (F_x_r * R) / I_R; % wheel rotary acceleration
 beta_dot(isnan(beta_dot)) = 0; % side slip angle well defined? > recover side slip angle
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
