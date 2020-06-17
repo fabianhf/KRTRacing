e0e495b606d7117fda146795e997d271b7a2ecaf
Steffen Zeile <steffen@zeilehome.de>
2020-06-18 01:15:33 +0200

    Simulate full length

--- matfiles/Controller/Simulate.m
+++ matfiles/Controller/Simulate.m
@@ -67,7 +67,7 @@ problem = falcon.Problem('KRTRacing');
 
 % Specify Discretization
 sStart = 0;
-sEnd = 450;
+sEnd = s(end);
 delta_s = 0.5;
 n = round((sEnd - sStart)/delta_s) + 1;
 tau = linspace(0,1,n);
