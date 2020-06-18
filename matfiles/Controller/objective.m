a10b550d6bf8cff8ef3a21a12ef4ebf23e4c0693
fabianhf <fabian.haas-fickinger@rwth-aachen.de>
2020-06-18 06:47:38 +0200

    Reverted beta limit to 20 and introduced delta_dot limit

--- matfiles/Controller/objective.m
+++ matfiles/Controller/objective.m
@@ -7,7 +7,7 @@ global k3
 k1 = 1e-5;
 k2 = 1e-0;
 if(isempty(k3))
-    k3 = 1e-5;      % A change in this variable has only an effect on the first run after the start of Matlab or after "clear global"
+    k3 = 1e-8;      % A change in this variable has only an effect on the first run after the start of Matlab or after "clear global"
 end
 
 objective_dot = 1 + k3 .* beta.^2; % + k2.*fB; % + k1.*delta.^2;
